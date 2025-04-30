36d203112c20a1f7416b23caf29f5e73308f0bb6
Selvakumar <vselva@gmail.com>
2025-04-30 17:15:56 +0530

    Update call-apply-bind.js

--- call-apply-bind/call-apply-bind.js
+++ call-apply-bind/call-apply-bind.js
@@ -9,17 +9,17 @@ let person = {
     name: 'Selva'
 };
 let message = 'Good Morning!';
-// call
+// call - Syntax: function.call(thisArg, arg1, arg2, ...)
 // this can be passed as first argument as object
 // function argument should be passed as comman separated after the first object argument 
 greet.call(person, message);
 
-// apply 
+// apply - Syntax: function.apply(thisArg, [arg1, arg2, ...])
 // this can be passed as first argument as object
 // function arguments should be passed in array 
 greet.apply(person, [message]);
 
-// bind
+// bind - Syntax: function.bind(thisArg, arg1, arg2, ...)
 // this can be passed as first argument as object
 // function arguments passed as comma separated after first argument 
 // but this will not executed immediately but returns another function which need tobe called. 
