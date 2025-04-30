8df83d66988b7dfb2f7ce7acacc4a82a3edfb120
Selvakumar <vselva@gmail.com>
2025-04-30 17:23:03 +0530

    Update call-apply-bind.js

--- call-apply-bind/call-apply-bind.js
+++ call-apply-bind/call-apply-bind.js
@@ -5,23 +5,24 @@ function greet(message) {
 greet('Good Morning'); // this.name will be undefined 
 
 // this object and function argument initialization 
-let person = {
+let thisArg = {
     name: 'Selva'
 };
-let message = 'Good Morning!';
+let messageArg = 'Good Morning!';
+
 // call - Syntax: function.call(thisArg, arg1, arg2, ...)
 // this can be passed as first argument as object
 // function argument should be passed as comman separated after the first object argument 
-greet.call(person, message);
+greet.call(thisArg, messageArg);
 
 // apply - Syntax: function.apply(thisArg, [arg1, arg2, ...])
 // this can be passed as first argument as object
 // function arguments should be passed in array 
-greet.apply(person, [message]);
+greet.apply(thisArg, [messageArg]);
 
 // bind - Syntax: function.bind(thisArg, arg1, arg2, ...)
 // this can be passed as first argument as object
 // function arguments passed as comma separated after first argument 
 // but this will not executed immediately but returns another function which need tobe called. 
-let bindedFunction = greet.bind(person, message);
+let bindedFunction = greet.bind(thisArg, messageArg);
 bindedFunction();
