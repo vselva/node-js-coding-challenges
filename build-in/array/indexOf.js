eea1dc1ec629414b3a74b0fab0d009ae08525055
Selvakumar <vselva@gmail.com>
2025-05-28 13:28:40 +0530

    Create indexOf.js

---
+++ build-in/array/indexOf.js
@@ -0,0 +1,3 @@
+// indexOf is useful to find the index of the first available char in array
+const array = ['a', 'b', 'c', 'd', 'e'];
+console.log('index of "c" in', array, 'is:', array.indexOf('c'));
