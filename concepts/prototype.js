ed2b88338e18ca69728233e2cef056c48a1ffd98
Selvakumar <vselva@gmail.com>
2025-05-28 13:49:02 +0530

    Update prototype.js

--- concepts/prototype.js
+++ concepts/prototype.js
@@ -18,3 +18,19 @@ console.log(person1.greetUser());   // person1 doesn't have a greet() method,
                                     // inherits it from Person.prototype.greetUser
 console.log(person1.age); // person1 doesn't have a age property, 
                           // inherits it from Person.prototype.age
+
+console.log('');
+console.log('custom implementation of find with prototype:');
+Array.prototype.myFind = function(cb) {
+    for (let i = 0; i < this.length; i++) {
+        if (cb(this[i], i, this)) {
+            return this[i];
+        }
+    }
+    return undefined;
+};
+const arr = [1, 3, 5, 7, 8, 9, 10];
+const firstEven = arr.myFind(el => el % 2 === 0 );
+console.log('First Even Number in Array with custom Prototype function:', firstEven);
+
+console.log('-----');
