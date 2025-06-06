68a018f3972f6ab3a50828440b38b7320a304705
Selvakumar <vselva@gmail.com>
2025-06-06 13:21:58 +0530

    Added Special Wrapper Function

---
+++ concepts/special-wrapper-function.js
@@ -0,0 +1,15 @@
+console.log('Hi');
+exports.add = (a, b) => a + b;
+exports.sub = (a, b) => a - b;
+
+console.log('');
+console.log('=========== Exports ===========');
+console.log('Special Wrapper Function:');
+console.log('arguments -', arguments);
+
+// exports
+console.log('');
+console.log('=========== Exports ===========');
+console.log('arguments[0]:', arguments[0]);
+console.log('exports:', exports); 
+
