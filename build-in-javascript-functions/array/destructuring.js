251a540b779928f2c025d0b10ecce2a4e79d6a8d
Selvakumar <vselva@gmail.com>
2025-07-12 12:33:32 +0530

    Update destructuring.js

--- build-in-javascript-functions/array/destructuring.js
+++ build-in-javascript-functions/array/destructuring.js
@@ -2,3 +2,6 @@
 const array = [1, 2, 3, 4, 5];
 const [ first, second, ...restOfArray ] = array;
 console.log('org array:', array, 'first:', first, 'second:', second, ' rest of array items:', restOfArray);
+
+const [f, , t] = array;
+console.log('first:', f, 'third:', t, '(second array item is missed while destructuring');
