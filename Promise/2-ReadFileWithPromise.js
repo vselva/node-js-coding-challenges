9fe98ba27d615d49df99fce981faf279437a9319
Selvakumar <vselva@gmail.com>
2025-07-29 17:00:52 +0530

    Refactor to use double quotes for strings
    Updated all string literals to use double quotes instead of single quotes for consistency throughout the file.

--- Promise/2-ReadFileWithPromise.js
+++ Promise/2-ReadFileWithPromise.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Promise to handle reading a file
 const fileReadPromise = (path) => {
     return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf-8', (err, data) => {
-            if (err) reject(err);  // Reject on error
-            else resolve(data);     // Resolve with file data on success
+        fs.readFile(path, "utf-8", (err, data) => {
+            if (err) reject(err); // Reject on error
+            else resolve(data); // Resolve with file data on success
         });
     });
 };
 
 // Call the function and handle the promise
-fileReadPromise('./test.txt')
+fileReadPromise("./test.txt")
     .then((data) => {
-        console.log('File read is successful. Data: ' + data);
+        console.log("File read is successful. Data: " + data);
     })
     .catch((err) => {
-        console.log('File read failed. Error: ' + err);
+        console.log("File read failed. Error: " + err);
     })
     .finally(() => {
-        console.log('Finally done!');
+        console.log("Finally done!");
     });
