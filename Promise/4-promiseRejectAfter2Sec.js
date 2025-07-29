985dd549be40f16a97131f0299176027cee39000
Selvakumar <vselva@gmail.com>
2025-07-29 17:03:56 +0530

    Clean up promise rejection example code
    Removed commented-out code and unnecessary comments for clarity. Reformatted promise chaining for improved readability.

--- Promise/4-promiseRejectAfter2Sec.js
+++ Promise/4-promiseRejectAfter2Sec.js
@@ -1,17 +1,11 @@
-
 prom = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve('Resolved after two seconds!');
-        reject('Rejected after two seconds!');
+        reject("Rejected after two seconds!");
     }, 2000);
 });
 
-prom
-    .then(console.log) // equivalent to (data) => { console.log(data) }; 
-    .catch(console.log)  // equivalent to (data) => { console.log(data) }; 
+prom.then(console.log)
+    .catch(console.log)
     .finally(() => {
-    console.log('Finally done!')
-});
-
-// .then(console.log) automatically passes the resolved value to console.log(data).
-// .catch(console.log) automatically passes the rejected error to console.log(error).
+        console.log("Finally done!");
+    });
